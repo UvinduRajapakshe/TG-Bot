74692e9fa6c70f01cb204e9e9891393c17b6d1c4
Uvindu Bro <79355885+UvinduBro@users.noreply.github.com>
2021-07-19 13:29:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import telebot
 
 
-bot = telebot.TeleBot("1939991142:AAGf4Jd6fANpne1YU2zCMT_nsquEQefSwgo")
+bot = telebot.TeleBot("1939991142:AAFHG4lglbEmM91Em0RmXpKuH64v2gEnx0w")
 
 
 @bot.message_handler(commands=["start"])
