ba666b6eb98a85fbe961d6a22e2f25fb6cf3d499
Uvindu Bro <79355885+UvinduBro@users.noreply.github.com>
2021-07-19 13:23:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ bot = telebot.Telebot("1939991142:AAGf4Jd6fANpne1YU2zCMT_nsquEQefSwgo")
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-bot.reply_to(message, "Hello! I'm Uvindu Bro Chat Bot")
+  bot.reply_to(message, "Hello! I'm Uvindu Bro Chat Bot")
 
 
 @bot.message_handler(commands=["how"])
-def send_message(message)
-bot.send_message(message, "https://youtube.com/c/Uvindubro")
+def send_message(message):
+  bot.send_message(message, "https://youtube.com/c/Uvindubro")
+
 
 
 bot.polling()
